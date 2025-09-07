2f01fc725b5b69edeaa0804c706e738031bcd052
ramzy2000 <caseyduffy00@gmail.com>
2025-09-07 14:55:16 -0500

    update system-manager
    add update method to system manager
    add a vector of system pointers
    add a gameData shared pointer

--- src/Entity/System/SystemManager.cpp
+++ src/Entity/System/SystemManager.cpp
@@ -1,2 +1,11 @@
 #include "SystemManager.h"
 
+SystemManager::SystemManager(std::shared_ptr<GameData> gameData)
+{
+	this->gameData = gameData;
+}
+
+void SystemManager::Update(std::shared_ptr<Entity> entity, sf::Time deltaTime)
+{
+    // update all systems for this entity
+}
--- src/Entity/System/SystemManager.h
+++ src/Entity/System/SystemManager.h
@@ -1,10 +1,28 @@
 #ifndef SYSTEM_MANAGER_H
 #define SYSTEM_MANAGER_H
 
+#include "System.h"
+#include "../../Game/GameData.h"
+#include "../Entity.h"
+#include <array>
+#include <vector>
+
+// Store a list of pointers to each system.
+
+// I want to process each system in one frame time.
+// I dont want to have to itterate over each systems entites.
+
+// will this solve this problem
+
+static const unsigned int MAX_SYSTEMS = 10;
+
 class SystemManager
 {
+    std::vector<std::shared_ptr<System>> systems;
+    std::shared_ptr<GameData> gameData;
 public:
-    
+    SystemManager(std::shared_ptr<GameData> gameData);
+    void Update(std::shared_ptr<Entity> entity, sf::Time deltaTime);
 };
 
 #endif //!SYSTEM_MANAGER_H
