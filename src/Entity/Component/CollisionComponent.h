9932e3c28d0d0862aef47465d31f51018b1d09d0
ramzy2000 <caseyduffy00@gmail.com>
2025-09-12 17:05:50 -0500

    create collision component

---
+++ src/Entity/Component/CollisionComponent.h
@@ -0,0 +1,13 @@
+#ifndef COLLISION_COMPOENT_H
+#define COLLISION_COMPOENT_H
+#include <SFML/Graphics.hpp>
+
+#include "Component.h"
+class CollisionComponent : public Component
+{
+
+public:
+	CollisionComponent();
+};
+
+#endif //!COLLISION_COMPOENT_H
