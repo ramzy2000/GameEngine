6cd56b955348c4ec0e8472c1a49746c05a90ea91
ramzy2000 <caseyduffy00@gmail.com>
2025-09-12 17:30:40 -0500

    add collision component

--- src/Entity/Component/CollisionComponent.h
+++ src/Entity/Component/CollisionComponent.h
@@ -2,12 +2,20 @@
 #define COLLISION_COMPOENT_H
 #include <SFML/Graphics.hpp>
 
+class SpriteComponent;
+
 #include "Component.h"
 class CollisionComponent : public Component
 {
-
+	sf::FloatRect bounds;
 public:
 	CollisionComponent();
+
+	bool checkCollision(const CollisionComponent& other) const;
+
+	void updateBounds(SpriteComponent& spriteComponent);
+
+	const sf::FloatRect& getBounds() const;
 };
 
 #endif //!COLLISION_COMPOENT_H
