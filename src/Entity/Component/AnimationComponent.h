6f047b0ce73c342551d6022715f00d316f2c865a
ramzy2000 <caseyduffy00@gmail.com>
2025-09-18 21:22:22 -0500

    create animation component

---
+++ src/Entity/Component/AnimationComponent.h
@@ -0,0 +1,11 @@
+#ifndef ANIMATION_COMPONENT_H
+#define ANIMATION_COMPONENT_H
+
+#include "Component.h"
+class AnimationComponent : public Component
+{
+public:
+
+};
+
+#endif // !ANIMATION_COMPONENT_H
