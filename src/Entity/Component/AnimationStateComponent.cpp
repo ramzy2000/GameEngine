833dbdc1cdcad868e2d5f4a6842c78afe31d6c8f
cduffy-tri <caseyduffy00@gmail.com>
2025-09-20 13:37:32 -0500

    create animation state component

---
+++ src/Entity/Component/AnimationStateComponent.cpp
@@ -0,0 +1,5 @@
+#include "AnimationStateComponent.h"
+
+AnimationStateComponent::AnimationStateComponent()
+{
+}
---
+++ src/Entity/Component/AnimationStateComponent.h
@@ -0,0 +1,11 @@
+#ifndef ANIMATION_STATE_COMPONENT_H
+#define ANIMATION_STATE_COMPONENT_H
+
+#include "Component.h"
+class AnimationStateComponent : public Component
+{
+public:
+	AnimationStateComponent();
+};
+
+#endif // !ANIMATION_STATE_COMPONENT_H
